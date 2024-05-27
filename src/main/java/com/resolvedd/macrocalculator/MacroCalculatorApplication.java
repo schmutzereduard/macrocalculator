69d0d154eaa1bebff3a9aef184e262769e70f486
Schmutzer Eduard <schmutzer.eduard@gmail.com>
2024-05-27 12:54:35 +0300

    First Commit

---
+++ src/main/java/com/resolvedd/macrocalculator/MacroCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.resolvedd.macrocalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroCalculatorApplication.class, args);
+	}
+
+}
