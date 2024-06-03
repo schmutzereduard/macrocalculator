fa7dcff307d2377c595ef399cba65f21dc44579b
Schmutzer Eduard <schmutzer.eduard@gmail.com>
2024-06-03 21:32:31 +0300

    added cross origin to FE for food-types

--- src/main/java/com/resolvedd/macrocalculator/controller/FoodTypeController.java
+++ src/main/java/com/resolvedd/macrocalculator/controller/FoodTypeController.java
@@ -1,6 +1,7 @@
 package com.resolvedd.macrocalculator.controller;
 
 import com.resolvedd.macrocalculator.model.FoodType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/food-types")
 public class FoodTypeController {
